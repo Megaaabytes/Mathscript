282bfb812644d821206d1ce8b74dae100efc6ad3
Megaaabytes <73676100+Megaaabytes@users.noreply.github.com>
2021-10-24 11:52:43 -0400

    Mathscript Runtime 1.0.0.3

---
+++ Parsing/ConvertVariableNameToValue.cs
@@ -0,0 +1,13 @@
+﻿using Math_Script_Runtime_Environment.InstructionsTools;
+using Math_Script_Runtime_Environment.Variables;
+
+namespace Math_Script_Runtime_Environment.Parsing
+{
+    public static class ConvertVariableNameToValue
+    {
+        public static VariableInformation ParseValue(this string mess, int i)
+        {
+            return ReadInstructions.ReadVariable(mess.Replace("${", "").Replace("}", ""), i, ReadInstructions.variables.ToArray(), ReadInstructions.arrayVariables.ToArray());
+        }
+    }
+}
