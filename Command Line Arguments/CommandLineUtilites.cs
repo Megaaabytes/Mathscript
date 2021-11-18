9af3b0ca7df3e32e48567af93a1cfe70bdace32b
Megaaabytes <73676100+Megaaabytes@users.noreply.github.com>
2021-11-17 21:51:01 -0500

    Mathscript Runtime 1.0.0.4

---
+++ Command Line Arguments/CommandLineUtilites.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Math_Script_Runtime_Environment.Command_Line_Arguments
+{
+    public class CommandLineUtilites
+    {
+        public static int GetCommandLineArgumentsLength(string[] args)
+        {
+            List<string> finalArgs = new List<string>();
+            try
+            {
+                for (int i = 1; i < args.Length; i++)
+                {
+                    finalArgs.Add(args[i]);
+                }
+            }
+            catch
+            {
+                return 0;
+            }
+            return finalArgs.Count;
+        }
+
+        public static string[] GetCommandLineArguments(string[] args)
+        {
+            List<string> finalArgs = new List<string>();
+            try
+            {
+                for (int i = 1; i < args.Length; i++)
+                {
+                    finalArgs.Add(args[i]);
+                }
+            }
+            catch
+            {
+                return null;
+            }
+            return finalArgs.ToArray();
+        }
+    }
+}
---
+++ Operators/Operator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Math_Script_Runtime_Environment.Variables;
+
+namespace Math_Script_Runtime_Environment.Operators
+{
+    public class Operator
+    {
+        public string arg1 { get; set; }
+        public string arg2 { get; set; }
+        public string arg3 { get; set; }
+        public OperatorType type { get; }
+        public OperatorVariableType varType { get; }
+
+        public Operator(string arg1, string arg2, OperatorType type, OperatorVariableType varType)
+        {
+            this.arg1 = arg1;
+            this.arg2 = arg2;
+            this.type = type;
+            this.varType = varType;
+        }
+
+        public void PrintOperator()
+        {
+            Console.WriteLine($"{arg1},{arg2},{arg3}:{type}");
+        }
+
+        public void SetArg3(string data)
+        {
+            if (arg3 == null)
+            {
+                arg3 = data;
+            }
+            else
+            {
+                throw new UnauthorizedAccessException("You cannot change arg3 once it has already been defined.");
+            }
+        }
+    }
+
+    public enum OperatorVariableType
+    {
+        Default,
+        String,
+        Number,
+        Boolean,
+        Character,
+    }
+
+    public enum OperatorType
+    {
+        Plus,
+        Minus,
+        Multiply,
+        Divide,
+        Power,
+        Lessthan,
+        Greaterthan,
+        Equals,
+        Modulo,
+        Not,
+    }
+}
