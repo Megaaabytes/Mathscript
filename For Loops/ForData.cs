1e1f9a3ad205f385c0d365e7e6d0f03914b2219e
Megaaabytes <73676100+Megaaabytes@users.noreply.github.com>
2021-11-19 21:02:24 -0500

    Mathscript Runtime 1.0.0.5

---
+++ For Loops/ForData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Math_Script_Runtime_Environment.For_Loops
+{
+    public struct ForData
+    {
+        public string arrayVariableName { get; }
+        public string objectName { get; }
+        public string index { get; }
+
+        public ForData(string arrayVariableName, string objectName, string index)
+        {
+            this.arrayVariableName = arrayVariableName;
+            this.objectName = objectName;
+            this.index = index;
+        }
+    }
+
+    public enum ForType
+    {
+        For,
+        BFor,
+    }
+}
